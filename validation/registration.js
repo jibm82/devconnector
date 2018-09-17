a9e8d2d9de0e536dfc564fa91303cd53542b845d
Jorge Baez <jibm_82@yahoo.com.mx>
2018-09-17 17:20:45 -0500

    Implemented login & registration validations

---
+++ validation/registration.js
@@ -0,0 +1,44 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateRegistrationInput(data) {
+    let errors = {};
+
+    data.name = !isEmpty(data.name) ? data.name : '';
+    data.email = !isEmpty(data.email) ? data.email : '';
+    data.password = !isEmpty(data.password) ? data.password : '';
+    data.password_confirmation = !isEmpty(data.password_confirmation) ? data.password_confirmation : '';
+
+    if (!Validator.isLength(data.name, { min: 2, max: 30 })) {
+        errors.name = 'Name must be between 2 and 30 characters';
+    }
+
+    if (Validator.isEmpty(data.name)) {
+        errors.name = 'Name field is required';
+    }
+
+    if (Validator.isEmpty(data.email)) {
+        errors.email = 'Email field is required';
+    }
+
+    if (!Validator.isEmail(data.email)) {
+        errors.email = 'Email is invalid';
+    }
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'Password field is required';
+    }
+
+    if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+        errors.password = 'Password must be at least 6 characters';
+    }
+
+    if (!Validator.equals(data.password, data.password_confirmation)) {
+        errors.password_confirmation = 'Passwords must match';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    };
+}
