e2e7e4899b68577c71ab71c8925eea247d4dbe12
Jorge Baez <jibm_82@yahoo.com.mx>
2018-09-17 16:43:21 -0500

    Implemented authentication

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport')
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -21,12 +22,16 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
 // Use routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
-
-app.get('/', (req, res) => res.send('Hello World'));
 
 const port = process.env.PORT || 5000;
 
