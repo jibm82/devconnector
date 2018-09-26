f9f65baa1b7cfbc5a10ea363bde9e0ec8f0a48e8
Jorge Baez <jibm_82@yahoo.com.mx>
2018-09-25 22:58:52 -0500

    Error Handling & Display

--- validation/registration.js
+++ validation/registration.js
@@ -25,16 +25,20 @@ module.exports = function validateRegistrationInput(data) {
         errors.email = 'Email is invalid';
     }
 
-    if (Validator.isEmpty(data.password)) {
-        errors.password = 'Password field is required';
-    }
-
     if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
         errors.password = 'Password must be at least 6 characters';
     }
 
+    if (Validator.isEmpty(data.password)) {
+        errors.password = 'Password field is required';
+    }
+
     if (!Validator.equals(data.password, data.password_confirmation)) {
         errors.password_confirmation = 'Passwords must match';
+    }
+
+    if (Validator.isEmpty(data.password_confirmation)) {
+        errors.password_confirmation = 'Password Confirmation field is required';
     }
 
     return {
