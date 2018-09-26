e33e3163a742f6968ff45c328311bbc749f184ee
Jorge Baez <jibm_82@yahoo.com.mx>
2018-09-26 01:14:17 -0500

    Initial profile reducer

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 
 import { Provider } from "react-redux";
 import store from './store';
 
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
+import Dashboard from "./components/dashboard/Dashboard";
 import Footer from "./components/layout/Footer";
-import Register from "./components/auth/Register";
+import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
+import Navbar from "./components/layout/Navbar";
+import Register from "./components/auth/Register";
 
 import "./App.css";
 
@@ -33,8 +35,8 @@ if (localStorage.jwtToken) {
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     store.dispatch(logoutUser());
-    // TODO: Clear current profile
-
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
     // Redirect to login
     window.location.href = "/login";
   }
@@ -51,6 +53,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
+import { clearCurrentProfile } from "../../actions/profileActions";
 
 class Navbar extends Component {
   onLogoutClick(e) {
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
     window.location.href = "/";
   }
@@ -80,12 +82,13 @@ class Navbar extends Component {
 }
 
 Navbar.propTypes = {
-  logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  clearCurrentProfile: PropTypes.func.isRequired,
+  logoutUser: PropTypes.func.isRequired
 }
 
 const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { logoutUser })(Navbar);
+export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(Navbar);
